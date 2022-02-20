5d3bb4904b28a7fb5835d7304bcc30a5ae212db2
Andy Pickle <pickleat@gmail.com>
2022-02-20 12:00:49 -0600

    adding signout component

---
+++ src/components/SignOut.vue
@@ -0,0 +1,27 @@
+<template>
+   <button class="button" @click="signOut" :disabled="loading">
+      Sign Out
+    </button> 
+</template>
+
+<script>
+export default {
+  setup() {
+    async function signOut() {
+      try {
+        loading.value = true
+        let { error } = await supabase.auth.signOut()
+        if (error) throw error
+      } catch (error) {
+        alert(error.message)
+      } finally {
+        loading.value = false
+      }
+    }
+
+    return {
+      signOut
+    }
+  },
+}
+</script>
